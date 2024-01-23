f0aa0d9da355f5db35da1a20b97dd31c08574746
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-24 00:12:14 +0300

    overrides attack and bleed methods in Werewolf subclass

--- src/rpg/models/Werewolf.java
+++ src/rpg/models/Werewolf.java
@@ -1,8 +1,25 @@
 package rpg.models;
 
-public class Werewolf extends Monster{
+import rpg.utils.Constants;
+
+public class Werewolf extends Monster {
 
     public Werewolf(String name, int hitPoints, double damage) {
         super(name, hitPoints, damage);
+    }
+
+    @Override
+    public double bleed() {
+        return getDamage() * Constants.BLEED_PERCENTAGE;
+    }
+
+    @Override
+    public double bleed(int level) {
+        return getDamage() * level * Constants.BLEED_PERCENTAGE;
+    }
+
+    @Override
+    public double attack() {
+        return getDamage() + bleed();
     }
 }
