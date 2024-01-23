094c8680ef78911a2fb77461715f90cc3cbb6030
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-23 23:50:56 +0300

    tests productForSale subclasses

--- src/Store.java
+++ src/Store.java
@@ -1,10 +1,24 @@
+import store.models.Bread;
+import store.models.Chocolate;
+import store.models.Coke;
+import store.models.ProductForSale;
+
 public class Store {
     public static void main(String[] args) {
 
-        listProducts();
+
+        ProductForSale[] products = new ProductForSale[5];
+        products[0] = new Chocolate("dessert", 30.5, "Handmade Chocolate", "white", Boolean.TRUE);
+        products[1] = new Bread("bakery", 10, "zero sugar", "white", "brown");
+        products[2] = new Coke("beverage", 25, "extremely unhealthy but delicious", Boolean.TRUE, 1);
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for (ProductForSale product : products) {
+            if (product != null) {
+                product.showDetails();
+            }
+        }
     }
 }
