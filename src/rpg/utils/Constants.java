6721bffec008781f5dcf62df25e4756fc9a89a51
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-24 00:05:23 +0300

    adds constants of game

---
+++ src/rpg/utils/Constants.java
@@ -0,0 +1,9 @@
+package rpg.utils;
+
+public class Constants {
+    private Constants() {
+    }
+
+    public static final double POISON_PERCENTAGE = 0.3;
+    public static final double BLEED_PERCENTAGE = 0.25;
+}
