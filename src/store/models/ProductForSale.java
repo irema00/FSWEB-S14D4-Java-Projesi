4c7e97d425ddbee5342647233bfd41d542034a06
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-23 23:36:36 +0300

    completes ProductForSale abstract class

---
+++ src/store/models/ProductForSale.java
@@ -0,0 +1,36 @@
+package store.models;
+
+public abstract class ProductForSale {
+    private String type;
+    private double price;
+    private String description;
+
+    public double getSalesPrice(int quantity) {
+        return quantity + getPrice();
+    }
+
+    ;
+
+    public abstract void showDetails();
+
+    public ProductForSale(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getType() {
+
+        return type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+
+        return description;
+    }
+
+}
